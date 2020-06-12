bc45279244f5eadec33de23f95fd2d06f0a45644
Mark Rea <markrea366@gmail.com>
2020-06-12 19:57:03 +0100

    created weather summary and summaries components

---
+++ src/__tests__/components/forcast-summary.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { cleanup, render } from '@testing-library/react';
+import ForecastSummary from '../../components/forcast-summary';
+
+describe("ForecastSummary", () => {
+    it("renders correctly", () => {
+        const { asFragment } = render(
+            <ForecastSummary
+                date="mockDate"
+                temperature="mockTemperature"
+                description="mockDescription"
+                icon="mockIcon"
+            />
+        );
+        expect(asFragment).toMatchSnapshot();
+    });
+
+    it("renders the correct props", () => {
+        const { getByText } = render(
+            <ForecastSummary
+            date="mockDate"
+            temperature="mockTemperature"
+            description="mockDescription"
+            icon="mockIcon"
+            />
+        );
+        expect(getByText("mockDate")).toHaveClass("date");
+        expect(getByText("mockTemperature")).toHaveClass("temperature");
+        expect(getByText("mockDescription")).toHaveClass("description");
+        expect(getByText("mockIcon")).toHaveClass("icon");
+    });
+});
